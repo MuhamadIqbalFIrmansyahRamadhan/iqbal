4d5a87dce9820b3f3a345c8298bcefdf08c60837
Muhamad Iqbal Firmansyah Ramadhan <145944982+MuhamadIqbalFIrmansyahRamadhan@users.noreply.github.com>
2024-03-25 14:39:03 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+#buat program untuk menampilkan biodta
+
+nama=("juli awan ashari")
+nim=("SI20230017")
+semester=("dua")
+umur=int(19)
+alamat=("lombas,des,pajangan,kopang")
+lulusan=("smki ad-duha")
+
+print("=="*10)
+print("selamat datang",nama)
+print("nim anda",nim)
+print("anda semester",semester)
+print("umur anda",umur)
+print("alamat anda",alamat)
+print("lulusan sekolah",lulusan)
+print("=="*10)
